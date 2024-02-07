92edd2e7e835681856335ba28bff058c6bb7d6b7
Madison Leora Cook <cook441@purdue.edu>
2024-02-07 11:54:33 -0500

    11:54

--- Lab4Restart/Lab4Restart.cpp
+++ Lab4Restart/Lab4Restart.cpp
@@ -3,9 +3,81 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std; 
+
+// Print out the menu of choices for the user to select from
+void printMenu() {
+    cout << "Please Select which operation to perform: " << endl;
+    cout << "\t1. Factorial" << endl; 
+    cout << "\t2. Arithmetic Series" << endl; 
+    cout << "\t3. Geometric Series" << endl; 
+    cout << "\t4. Exit" << endl; 
+    cout << "Your Selection: "; 
+}
+void factorial() {
+    // write factorial code here
+}
+
+void arithmetic() {
+    // write arithmetic series code here
+}
+
+void geometric() {
+    //write geometric series here
+}
+
+
+
+int main() {
+    int choice; 
+    char again; 
+
+    do {
+        printMenu();
+        cin >> choice;
+
+        // quit if user chooses to exit (or any invalid choice) 
+        if (choice > 3 || choice < 1) {
+            return 0;
+        }
+
+        else if (choice == 1) {
+            factorial();
+            int n; 
+            long factorial = 1.0; 
+
+            cout << "Enter a number: "; 
+            cin >> n; 
+
+            if (n < 0)
+                cout << "Nice try, please enter a POSITIVE number...:"; 
+
+            else {
+                for (int i = 1; i <= n; ++i) {
+                    factorial *= i; 
+                }
+
+                cout << "Factorial of" << n << "=" << factorial; //change to show the math
+            }
+
+
+
+        }
+
+        else if (choice == 2) {
+            arithmetic();
+        }
+
+        else if (choice == 3) {
+            geometric();
+        }
+
+        cout << "\nGo Again? [Y/N]";
+        cin >> again;
+
+    } while (again == 'y' || again == 'Y'); 
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
